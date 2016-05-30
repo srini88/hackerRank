ef89f41493a6e2d3e86ac2a76b88b5ed9ff06ed0
srini88 <pvsrinivas.ece@gmail.com>
2016-05-29 19:02:13 -0500

    redundant

--- rocks.js
+++ rocks.js
@@ -1,47 +1,45 @@
-// https://www.hackerrank.com/challenges/gem-stones
-// John has discovered various rocks. Each rock is composed of various elements, and each element is represented by a lower-case Latin letter from 'a' to 'z'. An element can be present multiple times in a rock. An element is called a gem-element if it occurs at least once in each of the rocks.
 
-// Given the list of  rocks with their compositions, display the number of gem-elements that exist in those rocks.
+// var myArray = new Array();
+//  myArray['a'] = 200; 
+//  myArray['b'] = 300; 
+//  console.log(myArray)
+// [a: 200, b: 300]
 
-// Input Format
 
-// The first line consists of an integer, , the number of rocks. 
-// Each of the next  lines contains a rock's composition. Each composition consists of lower-case letters of English alphabet.
+// You can have an array of objects containing key-value pairs, like this:
 
-// Constraints 
- 
-// Each composition consists of only lower-case Latin letters ('a'-'z'). 
-//  length of each composition 
+// var per_kg_list = [
+//     {'03749': '3.000'},
+//     {'03750': '4.000'},
+//     {'03751': '5.000'}
+// ];
+// Alternatively, you can have an object containing key-value pairs, like this:
 
-// Output Format
+// var per_kg_list = {
+//     '03749': '3.000',
+//     '03750': '4.000',
+//     '03751': '5.000'
+// };
 
-// Print the number of gem-elements that are common in these rocks. If there are none, print 0.
+//http://stackoverflow.com/questions/37515959/how-to-create-an-associative-array-in-javascript-literal-notation
 
-// Sample Input
+    var myArray = [];
+     myArray['a'] = 200; 
+     myArray['b'] = 300; 
+     console.log(myArray); //prints [a: 200, b: 300]
 
-// 3
-// abcdde
-// baccd
-// eeabg
-// Sample Output
+//var elementArray = ['a': 200, 'b': 300];
+var rock  ="baccd";
 
-// 2
-// Explanation
+for (var s=0 ; s< elementArray.length ; ++s){ //looping through first rock
+    if (rock(elementArray[s]))
+        console.log(rock(elementArray[s]));
+    else
+        console.log("ledu")
+}
 
-// Only "a" and "b" are the two kinds of gem-elements, since these are the only characters that occur in every rock's composition.
 
-process.stdin.resume();
-process.stdin.setEncoding("ascii");
-_input = "";
-process.stdin.on("data", function (input) {
-    _input += input;
-});
 
-process.stdin.on("end", function () {
-   processData(_input);
-});
-
-var elementArray = [];
 
 //input passed from stdin
 // 3
@@ -49,43 +47,3 @@ var elementArray = [];
 // baccd
 // eeabg
 
-
-function processData(input) {
-
-    var rock;
-    var elements;
-    var rockCount = 0;
-    var data = input.split('\n');  //main array  split by new line
-    //[ '3', 'abcdde', 'baccd', 'eeabg' ]
-    var num = parseInt(data[0].trim(),10);  //This is n ...by default typeof getting string..so better to parseInt or use array.map(Number)
-    //now typeof gives number...and we converted it to number 3
-    rock = data[1];  //first rock..
-    //loop through first rock
-    for(var s=0;s<rock.length;s++){
-        //if we want to get actual frequency..do 
-        //elementArray[rock[s]] = (typeof elementArray[rock[s]] === 'undefined')? 1 : ++elementArray[rock[s]];
-        // gives //[ a: 1, b: 1, c: 1, d: 2, e: 1 ]
-        We dont care about actual frequency, we only care about if it is there or not...
-        //if it is not there, put 1 else override it to 1 again
-        elementArray[rock[s]] = 1;
-        //[ a: 1, b: 1, c: 1, d: 1, e: 1 ]
-    }
-    //now loop through next rocks..
-    for(var i=2; i<data.length; i++){
-        rock = data[i];   //assign that to data[i] //it will be a string each time //typeof gives string...
-        var added = [];
-        for(var s=0;s<rock.length;s++){
-            //elemetArray lo undi, added lo lekapothe , increment --may be found gem rock
-            if(elementArray[rock[s]] && !added[rock[s]]){  //addded to eliminate duplicates..
-               elementArray[rock[s]]++;      
-            };
-            added[rock[s]] = true;            
-        }
-    }
-    for(var key in elementArray){
-        if(elementArray[key] == num){
-            rockCount++;
-        }
-    }
-    console.log(rockCount);
-}

3662e34cc2215c78711e1214ee1a8a2f1cd71771
srini88 <pvsrinivas.ece@gmail.com>
2016-05-30 19:53:13 -0500

    character code conversions

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,46 +1,27 @@
+var str = "srinivas";
+var len = str.length;
+var arr=[];
+for (var i=0; i<len; ++i){
+    arr.push((str.charCodeAt(i)));
+}
+console.log(arr);  //[115, 114, 105, 110, 105, 118, 97, 115]
+ 
+// String.prototype.charCodeAt()   //this is on prootype , so you can do str.charCodeAt....
+// 'ABC'.charCodeAt(0); // returns 65
 
-// To encrypt the string, we need to rotate the alphabets in the string by a number k. If k is a multiple of 26, then rotating the alphabets in the string by k has no effect on the string
-// Rotation by k is same as rotation by k+26
-// So by taking modulo of k with 26, we get the number of times we need to rotate the alphabets of the string
-//  To rotate the alphabets in a string by a value k, we add k to the character. If this value exceeds the range of the alpabets, we need to wrap it back. The range of uppercase characters is from 65 ('A') to 90 ('Z'). The range of lowercase characters is from 97 ('a') to 122 ('z'). 
+// String.fromCharCode(num1[, ...[, numN]])
+// This method returns a string and not a String object.
 
-//  Example: For the string : "middle-Outz" and k=2 
-// We add 2 to 'm'. 'm' becomes 'o'. This value is within the ascii range so we don't need to wrap it. '-' remains unaltered. 'z' on adding 2 becomes 124.
+// Because fromCharCode() is a static method of String, you always use it as String.fromCharCode(), rather than as a method of a String object you created.
 
-function processData(input) {
-    //Enter your code here
-    var lines = input.split('\n');
-    var chars = parseInt(lines[0].trim(), 10);  //length of unencrypted string
-    var text = lines[1].trim();  //unaltered string
-    var shift = parseInt(lines[2].trim(), 10)%26;  //K
-    var output = "";
-    //make 93 as c ---c is 67  so do (93-90)+64  --same witt small letters
-    for(var i=0; i<chars; i++) {
-        var character = text.charCodeAt(i);
-        if (character >= 65 && character <= 90) {
-            character += shift;
-            if (character > 90) {
-                character = 64 + (character - 90);
-            }
-        } else if (character >= 97 && character <= 122) {
-            character += shift;
-            if (character > 122) {
-                character = 96 + (character - 122);
-            }
-        }
-        output += String.fromCharCode(character);
-    }
-    
-    process.stdout.write(output+"\n")
-} 
+// String.fromCharCode(65, 66, 67);  // "ABC"
 
-process.stdin.resume();
-process.stdin.setEncoding("ascii");
-_input = "";
-process.stdin.on("data", function (input) {
-    _input += input;
-});
+//now construct my name back from the arr...that has the ASCII codes of my name..
 
-process.stdin.on("end", function () {
-   processData(_input);
-});
+var myname="";
+
+for (var j=0; j < arr.length; ++j){
+    //console.log(String.fromCharCode(arr[j]))   //prints each character...s     r    i .....
+    myname += String.fromCharCode(arr[j]);
+}
+console.log(myname);  //srinivas

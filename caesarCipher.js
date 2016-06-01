2b7fe52369679bc9adc63661fdc59e8d901dc76c
srini88 <pvsrinivas.ece@gmail.com>
2016-06-01 14:18:38 -0500

    puzzle

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,8 +1,35 @@
-function highlight(event) {
-    event.stopPropagation();
-    alert(event.target.className);
-}
-var divs = document.getElementsByTagName('div');
-for (var i=0; i<divs.length;++i){
-    divs[i].addEventListener("click", highlight, false);
-}
+var arr1 = "john".split('');
+
+//console.log(arr1)  //["j", "o", "h", "n"]
+//console.log(arr1.length)  4
+var arr2 = arr1.reverse();
+
+//console.log(arr2)  //["n", "h", "o", "j"]
+//console.log(arr1)   //["n", "h", "o", "j"]  - reversed arr1 inplace...
+
+var arr3 = "jones".split('');
+//console.log(arr3)  //["j", "o", "n", "e", "s"]
+//console.log(arr1)  //["n", "h", "o", "j"] stiill same
+ 
+
+arr2.push(arr3);  //arr2 got modified , which modified arr1
+
+//console.log(arr1)  //now this prints ["n", "h", "o", "j", Array[5]]
+//console.log(arr2)   //["n", "h", "o", "j", Array[5]]
+//console.log(arr2[4])  //["j", "o", "n", "e", "s"]
+
+
+console.log(arr1.length)
+
+console.log("array 1: length=" + arr1.length + " last=" + arr1.slice(-1));
+console.log("array 2: length=" + arr2.length + " last=" + arr2.slice(-1));
+
+//https://www.toptal.com/javascript/interview-questions
+arr1 and arr2 are the same after the above code is executed for the following reasons:
+
+Calling an array object’s reverse() method doesn’t only return the array in reverse order, it also reverses the order of the array itself (i.e., in this case, arr1).
+The reverse() method returns a reference to the array itself (i.e., in this case, arr1). As a result, arr2 is simply a reference to (rather than a copy of) arr1. Therefore, when anything is done to arr2 (i.e., when we invoke arr2.push(arr3);), arr1 will be affected as well since arr1 and arr2 are simply references to the same object.
+And a couple of side points here that can sometimes trip someone up in answering this question:
+
+Passing an array to the push() method of another array pushes that entire array as a single element onto the end of the array. As a result, the statement arr2.push(arr3); adds arr3 in its entirety as a single element to the end of arr2 (i.e., it does not concatenate the two arrays, that’s what the concat() method is for).
+Like Python, JavaScript honors negative subscripts in calls to array methods like slice() as a way of referencing elements at the end of the array; e.g., a subscript of -1 indicates the last element in the array, and so on.

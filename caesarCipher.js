eab6afdc58aeb055b0a3d8729bc2d825f9d96824
srini88 <pvsrinivas.ece@gmail.com>
2016-06-04 15:47:00 -0500

    splice correct solutuin from end

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,16 +1,22 @@
 //var a=[0, 1, 0, 3, 2, 5];  //works for this..
 var a = [1, 0, 0, 1, 2, 5]; // [1, 0, 1, 2, 5, 0] does not work for this..
 
-var rem =[];
-for (var i=0 ; i <a.length ; ++i){
+// The problem has nothing to do with the return statement, the issue is your algorithm is wrong.
 
+// [0,0,1,2,3] will return [0,1,2,3,0]
+
+// When you loop in the positive direction and remove indexes, you skip indexes as the next index slides down to the position you already covered.
+
+// You need to loop in the negative direction. Start at the end and go to the start.
+
+///THis works perfect....splice remember to do it fromthe end of the loop..
+var rem =[];
+for (var i=a.length-1 ; i >=0 ; --i){
 
 	if (a[i]===0){
-		//index = i;
 		rem = a.splice(i, 1);
-		a.push(rem[0]);   ///problem is you remove it...it will alter the array..some cases the next item may be zero.we are missing these zeroes because..splice is mutable and will alter the index and move the zero to i... and then the next iteration i will be incremented..so splice is not right approach
+		a.push(rem[0]);   
 	}
 
 }
-
 console.log(a);

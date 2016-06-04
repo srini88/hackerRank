ff56434327fef5064cf21b6b380e3efe3e637c8d
srini88 <pvsrinivas.ece@gmail.com>
2016-06-04 15:34:26 -0500

    why using splice for moving indices does not work

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,31 +1,16 @@
+//var a=[0, 1, 0, 3, 2, 5];  //works for this..
+var a = [1, 0, 0, 1, 2, 5]; // [1, 0, 1, 2, 5, 0] does not work for this..
 
-//var a=[0, 1, 0, 3, 2, 5]
-var a=[1,2, 3, 4, 0, 0, 0, 0]
-
-/////nmove all the zeroes to the front...
-
-var zeroindex = a.length;  
-for(i= a.length-1; i>=0 ;i--)
-{
-    if(a[i]==0)
-    {
-        zeroindex = i;
-        break;
-    }  
-}
-
-console.log(zeroindex)
+var rem =[];
+for (var i=0 ; i <a.length ; ++i){
 
 
-for (i = zeroindex-1; i>=0 ; i--){
-    if (a[i]!= 0){
-        var temp;
+	if (a[i]===0){
+		//index = i;
+		rem = a.splice(i, 1);
+		a.push(rem[0]);   ///problem is you remove it...it will alter the array..some cases the next item may be zero.we are missing these zeroes because..splice is mutable and will alter the index and move the zero to i... and then the next iteration i will be incremented..so splice is not right approach
+	}
 
-        temp = a[i];
-        a[i] = a[zeroindex];
-        a[zeroindex] = temp;
-        zeroindex--;
-    }
 }
 
 console.log(a);
